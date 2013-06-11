8332ad4e4b81e6f3b656b30be7a59208b13c34a5
Everaldo Gomes <everaldo.gomes@gmail.com>
2013-06-11 19:42:02 +0000

    created project without TestUnit and ActiveRecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoppingList::Application.config.secret_token = '82d36b77edfe1d2927128dc4a91573cf80414ea7c857f04d809bd7435e326fcd3a38e927016655f0b1e686c11d257a6d7add4204a14a71839eebbb4dc0a2062b'
